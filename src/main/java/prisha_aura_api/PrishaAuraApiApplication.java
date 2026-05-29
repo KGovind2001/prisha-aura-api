440394824896519288f82a2d3b90b4dbd25cfedc
govind <govindkadgi05@gmail.com>
2026-05-29 11:40:41 +0530

    Initial first  commit

---
+++ src/main/java/prisha_aura_api/PrishaAuraApiApplication.java
@@ -0,0 +1,13 @@
+package prisha_aura_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrishaAuraApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrishaAuraApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/prisha_aura_api/PrishaAuraApiApplicationTests.java
@@ -0,0 +1,13 @@
+package prisha_aura_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrishaAuraApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
